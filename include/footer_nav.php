5c8d5782543f2998a06bb77243a95fc6862d0f50
ssysm <me@theeditorstudio.com>
2017-11-28 22:49:17 -0500

    init2

--- include/footer_nav.php
+++ include/footer_nav.php
@@ -2,8 +2,9 @@
     <ul class="tabs z-depth-1">
         <li class="tab col s3"><a routerLink="/" class="">首页</a></li>
         <li style="margin: auto" class="no-padding s8"><a href="sass.html">
-                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                    <i class="material-icons right">send</i>
+
+                <button style="background-color: #dfdfdf" class="btn blue-text text-darken-2" type="submit" name="action">搜索
+                    <i class="material-icons right">search</i>
                 </button>
             </a>
         </li>
