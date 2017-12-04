5f6e332377c98bea58abc42fef15a8adea8930d6
ssysm <me@theeditorstudio.com>
2017-12-03 22:57:34 -0500

    adding style change

--- include/top_nav.php
+++ include/top_nav.php
@@ -6,14 +6,14 @@
                 </div>
                 <a href="#"><img class="circle" style="height: 50%;width: 50%" src="./asset/img/user.png"></a>
                 <a href="#"><span class="white-text name" style="font-size: xx-large">某人</span></a>
-                <a href="#" style="display: inline"><span class="white-text">1 Following</span></a>
-                <a href="#" style="display: inline"><span class="white-text">2 Followers</span></a>
+                <a href="#" style="display: inline"><span class="white-text"><span style="font-size: x-large">1</span> Following</span></a>
+                <a href="#" style="display: inline"><span class="white-text"><span style="font-size: x-large">2</span> Followers</span></a>
             </div></li>
-        <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-        <li><a href="#!">Second Link</a></li>
+        <li><a href="#!"><i class="material-icons">menu</i>个人信息</a></li>
         <li><div class="divider"></div></li>
-        <li><a class="subheader">Subheader</a></li>
-        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+        <li><a class="subheader">操作</a></li>
+        <li><a href="#" id="login">登录</a></li>
+        <li><a href="#" id="login">注册</a></li>
     </ul>
     <div class="nav-wrapper">
         <a href="#" class="brand-logo center">主页</a>
--- index.php
+++ index.php
@@ -6,14 +6,29 @@ include('./include/top_nav.php');
 <div class="wrapper">
     <div class="container">
         <div class="feed">
-            <div class="card" style="padding: 10%">
+            <div class="card grey lighten-5 row z-depth-1" style="padding: 4vh;">
                 <div class="card-title">
-                    <span>I'm xxx</span>
+                        <div class="row valign-wrapper">
+                            <div class="col s2">
+                                <img src="./asset/img/user.png" alt="" class="circle responsive-img" style="display: inherit"> <!-- notice the "circle" class -->
+                            </div>
+                            <div class="col s10">
+                            <span class="black-text">
+                                xxx
+                            </span>
+                            </div>
+                        </div>
+                    <hr>
                 </div>
                 <div class="card-content">
                     <p>
                         Here is something123
                     </p>
+                </div>
+                <div class="card-action row">
+                    <a href="#" class="col s3 left">like</a>
+                    <a href="#" class="col s3 center">comment</a>
+                    <a href="#" class="col s3 right">share</a>
                 </div>
             </div>
             <div class="card" style="padding: 10%">
