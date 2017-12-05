05e7fef7d409ea91d0a5d4a61dce8d4aebc80e48
ssysm <me@theeditorstudio.com>
2017-12-04 22:18:27 -0500

    almost complete index page

--- include/header.php
+++ include/header.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="./asset/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/css/lightbox.min.css" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
--- index.php
+++ index.php
@@ -4,62 +4,51 @@ include('./include/top_nav.php');
 ?>
 <link rel="stylesheet" href="./asset/index.css">
 <div class="wrapper">
-    <div class="container">
-        <div class="feed">
-            <div class="card grey lighten-5 row z-depth-1" style="padding: 4vh;">
-                <div class="card-title">
-                        <div class="row valign-wrapper">
-                            <div class="col s2">
-                                <img src="./asset/img/user.png" alt="" class="circle responsive-img" style="display: inherit"> <!-- notice the "circle" class -->
-                            </div>
-                            <div class="col s10">
-                            <span class="black-text">
-                                xxx
-                            </span>
-                            </div>
+    <div class="container-wrap">
+        <div id="feed">
+            <div id="progress" style="">
+                <div class="preloader-wrapper active" style="width: 128px;height: 128px;margin: 0 auto;display: block;top: 30vh;">
+                    <div class="spinner-layer spinner-blue">
+                        <div class="circle-clipper left">
+                            <div class="circle"></div>
+                        </div><div class="gap-patch">
+                            <div class="circle"></div>
+                        </div><div class="circle-clipper right">
+                            <div class="circle"></div>
                         </div>
-                    <hr>
+                    </div>
+
+                    <div class="spinner-layer spinner-red">
+                        <div class="circle-clipper left">
+                            <div class="circle"></div>
+                        </div><div class="gap-patch">
+                            <div class="circle"></div>
+                        </div><div class="circle-clipper right">
+                            <div class="circle"></div>
+                        </div>
+                    </div>
+
+                    <div class="spinner-layer spinner-yellow">
+                        <div class="circle-clipper left">
+                            <div class="circle"></div>
+                        </div><div class="gap-patch">
+                            <div class="circle"></div>
+                        </div><div class="circle-clipper right">
+                            <div class="circle"></div>
+                        </div>
+                    </div>
+
+                    <div class="spinner-layer spinner-green">
+                        <div class="circle-clipper left">
+                            <div class="circle"></div>
+                        </div><div class="gap-patch">
+                            <div class="circle"></div>
+                        </div><div class="circle-clipper right">
+                            <div class="circle"></div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-content">
-                    <p>
-                        Here is something123
-                    </p>
-                </div>
-                <div class="card-action row">
-                    <a href="#" class="col s3 left">like</a>
-                    <a href="#" class="col s3 center">comment</a>
-                    <a href="#" class="col s3 right">share</a>
-                </div>
-            </div>
-            <div class="card" style="padding: 10%">
-                <div class="card-title">
-                    <span>I'm xxx</span>
-                </div>
-                <div class="card-content">
-                    <p>
-                        Here is something123
-                    </p>
-                </div>
-            </div>
-            <div class="card" style="padding: 10%">
-                <div class="card-title">
-                    <span>I'm xxx</span>
-                </div>
-                <div class="card-content">
-                    <p>
-                        Here is something123
-                    </p>
-                </div>
-            </div>
-            <div class="card" style="padding: 10%">
-                <div class="card-title">
-                    <span>I'm xxx</span>
-                </div>
-                <div class="card-content">
-                    <p>
-                        Here is something123
-                    </p>
-                </div>
+                <p style="font-size: x-large;display: table;margin: 0 auto">数据加载中...</p>
             </div>
         </div>
     </div>
@@ -68,3 +57,60 @@ include('./include/top_nav.php');
 include_once('./include/footer_nav.php');
 include_once('./include/footer.php');
 ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox.min.js"></script>
+<script>
+    var root = 'https://jsonplaceholder.typicode.com';
+    $.ajax({
+        url: root + '/posts',
+        method: 'GET'
+    }).then(function(data) {
+        $("#progress").hide()
+        for(var i = 0;i < data.length;i++){
+            var template = `
+            <div class="card grey lighten-5 row z-depth-1" style="padding:4vh;padding-bottom: 0;">
+                <div class="card-title">
+                        <div class="row valign-wrapper">
+                            <div class="col s2">
+                                <img src="./asset/img/user.png" alt="" class="circle responsive-img" style="display: inherit">
+            </div>
+            <div class="col s10">
+            <span class="black-text">
+               ${data[i].title}
+            </span>
+            </div>
+            </div>
+            <hr>
+            </div>
+            <div class="card-content">
+            <p>
+            ${data[i].body}
+            </p>
+            <div class="pic_grid">
+  <div class="pic_row">
+    <div class="pic_cell"><img src="http://via.placeholder.com/1920x1080" alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512" alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512"  alt="" /></div>
+  </div>
+  <div class="pic_row">
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512"  alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512" alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512" alt="" /></div>
+  </div>
+  <div class="pic_row">
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512"  alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512" alt="" /></div>
+    <div class="pic_cell"><img src="http://via.placeholder.com/512x512"  alt="" /></div>
+  </div>
+</div>
+            </div>
+            <div class="card-action" style="width: 100%;display: table;table-layout: fixed">
+            <a href="#" style="display: table-cell;text-align: left">Like</a>
+            <a href="#" style="display: table-cell;text-align: center">Comment</a>
+            <a href="#" style="display: table-cell;text-align: right">Share</a>
+            </div>
+            </div>
+        `
+            $("#feed").append(template);
+        }
+    });
+</script>
