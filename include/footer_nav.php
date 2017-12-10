d51a5f7e7dd6eb90574fa05c4d9e5cfbba89e15b
Shengming Yuan <syua9708@flhs.us>
2017-12-10 10:10:17 -0500

    adding tab

--- include/footer_nav.php
+++ include/footer_nav.php
@@ -12,8 +12,8 @@
 <div class="row footer container">
     <ul class="tabs z-depth-1">
         <li class="tab col s3"><a routerLink="/" class="">首页</a></li>
-        <li style="margin: auto" class="no-padding s8"><a href="sass.html">
-                <button style="background-color: #dfdfdf" class="btn blue-text text-darken-2" type="submit" name="action">搜索
+        <li style="margin: auto" class="no-padding s8"><a href="search.php">
+                <button style="background-color: #dfdfdf" class="btn blue-text text-darken-2" onclick="window.location.href = ('/search.php') ">搜索
                     <i class="material-icons right">search</i>
                 </button>
             </a>
